3d7187cc98f779067dec093f734d1367c7da68ad
James Higgins <james.milan.higgins@gmail.com>
2017-05-01 17:09:51 -0700

    add second passing test for id addition

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,8 +1,9 @@
 class Word
-  attr_accessor(:word)
+  attr_accessor(:word, :id)
   @@words = []
 
   define_method(:initialize) do |attributes|
     @word = attributes[:word]
+    @id = @@words.length + 1
   end
 end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,4 +1,5 @@
 
+require "word_definer"
 require "definition"
 require "rspec"
 require "pry"
@@ -7,5 +8,10 @@ describe("#word") do
   it ("returns the word") do
     test_word = Word.new({:word => "test"})
     expect(test_word.word).to(eq("test"))
+  end
+  it ("returns the first id") do
+    test_word = Word.new({:word => "test"})
+    expect(test_word.word).to(eq("test"))
+    expect(test_word.id).to(eq(1))
   end
 end
